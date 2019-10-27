a1926f4e2f2c39d80bf2e19152165277d9b38866
濱田早由里 <sayuri@hamadasayorisatonoMacBook-Pro.local>
2019-10-28 00:47:00 +0900

    [Update] メニューの開閉状態の管理、コンポーネントからのストアの利用

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="openSideMenu"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>マイアドレス帳</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
+    <sideNav></sideNav>
 
     <v-content>
     </v-content>
@@ -14,13 +15,20 @@
 </template>
 
 <script>
+import sideNav from './components/sideNav';
 export default {
   name: 'App',
   components: {
+    sideNav
   },
   data () {
     return {
       //
+    }
+  },
+  methods: {
+    openSideMenu(){
+      this.$store.dispatch('toggleSideMenu')
     }
   }
 }
