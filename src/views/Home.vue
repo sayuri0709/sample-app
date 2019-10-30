9561b12ab479326f0e4e97293b1a9dd2776f5e7e
sayuri.hamada <sayuri.hamada@gpol.co.jp>
2019-10-30 19:20:24 +0900

    [Update] Home画面を調整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,27 @@
 <template>
-  <HelloWorld />
+  <v-container text-xs-center justify-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>アドレス帳に連絡先を追加できます</h1>
+        <p>※追加された連絡先はリロードすると消去されます。</p>
+      </v-flex>
+
+      <v-flex xs12 mt-4 justify-center>
+          <router-link :to="{name: 'address_edit'}">
+              <v-btn color="info">連絡先追加</v-btn>
+          </router-link>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
   export default {
-    components: {
-      HelloWorld
-    }
   }
 </script>
+<style>
+  h1 + p {
+    margin-top: 10px;
+  }
+</style>
